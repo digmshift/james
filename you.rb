e1bf1adde18bb292babdbe2a9b9e11962aef9739
James Nichols <james@digmshift.com>
2015-05-06 08:50:04 -0400

    My First Ruby Program!

---
+++ you.rb
@@ -0,0 +1,17 @@
+puts "What is your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+
+puts "What is your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+puts "What city are you from?"
+city = get.chomp
+city.capitalize!
+
+puts "What state are you from?"
+state = gets.chomp
+state.upcase!
+
+print "Your name is #{first_name} #{last_name}, and you are from #{city}, #{state}"
